a388439ebacc90c8984efaa48a9163610e76a34c
baihao.01 <baihao.01@bytedance.com>
2019-08-02 18:27:48 +0800

    feat(algo): update No.47

--- 47_add.py
+++ 47_add.py
@@ -1,18 +1,23 @@
 # coding: utf-8
 
 
-def add(a, b):
-    sum = a ^ b
-    carry = (a & b) << 1
-
-    while carry != 0:
-        sum_tmp = sum ^ carry
-        carry_tmp = (sum & carry) << 1
-        sum, carry = sum_tmp, carry_tmp
-
-    return sum
+def add(self, num1, num2):
+    while num2:
+        result = (num1 ^ num2) & 0xffffffff
+        carry = ((num1 & num2) << 1) & 0xffffffff
+        num1 = result
+        num2 = carry
+        
+    if num1 <= 0x7fffffff:
+        result = num1
+        
+    else:
+        result = ~(num1 ^ 0xffffffff)
+        
+    return result
 
 
 if __name__ == '__main__':
     print add(5, 17)
     print add(142, 5312)
+    print add(1421, -43)
